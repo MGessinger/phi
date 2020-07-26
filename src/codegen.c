5b7d1237a6a7a011384dca63be915024b1704c38
Matthias Gessinger <s6magess@uni-bonn.de>
2020-07-26 18:23:05 +0200

    Generate Code for some basic binary operations

--- src/codegen.c
+++ src/codegen.c
@@ -6,27 +6,35 @@
 #include "parser.h"
 #include "codegen.h"
 
+static LLVMContextRef llvm_context;
+static LLVMModuleRef llvm_module;
+static LLVMBuilderRef llvm_builder;
+
 void initialiseLLVM ()
 {
 	LLVMPassRegistryRef passreg = LLVMGetGlobalPassRegistry();
 	LLVMInitializeCore(passreg);
+
+	llvm_context = LLVMGetGlobalContext();
+	llvm_builder = LLVMCreateBuilderInContext(llvm_context);
+	llvm_module = LLVMModuleCreateWithNameInContext("llvm_module", llvm_context);
 }
 
 void shutdownLLVM ()
 {
+	LLVMDisposeBuilder(llvm_builder);
+	LLVMDisposeModule(llvm_module);
 	LLVMShutdown();
 }
 
-LLVMValueRef codegenNumExpr (NumExpr *ne, LLVMContextRef ctx)
+LLVMValueRef codegenNumExpr (NumExpr *ne)
 {
-	LLVMTypeRef doubleType = LLVMDoubleTypeInContext(ctx);
+	LLVMTypeRef doubleType = LLVMDoubleTypeInContext(llvm_context);
 	return LLVMConstReal(doubleType, ne->val);
 }
 
-LLVMValueRef codegenBinaryExpr (BinaryExpr *be, LLVMContextRef ctx)
+LLVMValueRef codegenBinaryExpr (BinaryExpr *be)
 {
-	LLVMBuilderRef builder = LLVMCreateBuilderInContext(ctx);
-
 	LLVMValueRef l = codegen(be->LHS);
 	LLVMValueRef r = codegen(be->RHS);
 	if (l == NULL || r == NULL)
@@ -36,27 +44,34 @@ LLVMValueRef codegenBinaryExpr (BinaryExpr *be, LLVMContextRef ctx)
 	switch (be->op)
 	{
 		case '+':
-			val = LLVMBuildAdd (builder, l, r, "addtmp");
+			val = LLVMBuildFAdd(llvm_builder, l, r, "addtmp");
+			break;
+		case '-':
+			val = LLVMBuildFSub(llvm_builder, l, r, "subtmp");
+			break;
+		case '*':
+			val = LLVMBuildFMul(llvm_builder, l, r, "multmp");
+			break;
+		case '<':
+			val = LLVMBuildFCmp(llvm_builder, LLVMRealOLT, l, r, "lttmp");
 			break;
 		default:
 			val = logError("Unrecognized binary operator!", 0x2001);
 			break;
 	}
-	LLVMDisposeBuilder(builder);
 	return val;
 }
 
 LLVMValueRef codegen (Expr *e)
 {
-	LLVMContextRef llvmctx = LLVMGetGlobalContext();
 	if (e == NULL)
 		return NULL;
 	switch (e->expr_type)
 	{
 		case expr_number:
-			return codegenNumExpr(e->expr, llvmctx);
+			return codegenNumExpr(e->expr);
 		case expr_binop:
-			return codegenBinaryExpr(e->expr, llvmctx);
+			return codegenBinaryExpr(e->expr);
 		default:
 			logError("Cannot generate IR for unrecognized expression type!", 0x2000);
 	}
